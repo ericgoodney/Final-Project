ea9e5e5af6699eafe651b34e4708aac317930ee7
ericgoodney <42844640+ericgoodney@users.noreply.github.com>
2018-12-07 12:32:17 -0500

    Updated from Brython Server: 12/7/2018 12:32:17 PM

--- skijumper.py
+++ skijumper.py
@@ -7,13 +7,14 @@ Fork this repository to begin working on your final project. Then do the followi
 * Edit the file called "design.md" to describe the **design** of your project.
 * Add your python sources.
 * When the project is finished, make  pull request to let me know you're finished.
+"""
 from ggame import CircleAsset, LineStyle, Color, Sprite, App, RectangleAsset
 from math import sin,cos, tan, acos, asin, atan
 from math import exp, expm1, e, pi
 from math import log, log10, sqrt, log2
-"""
 
-"""
+myapp = App()
+
 red = Color(0xFF4040, 1.0)
 green = Color(0x00FF00, 1.0)
 blue = Color(0x1C86EE, 1.0)
@@ -24,11 +25,6 @@ thinline1 = LineStyle(1, black)
 thinline = LineStyle(1, blue)
 
 ball_asset = ImageAsset("Python Skiier.png", Frame(62,210,275,275),3)
-"""
-from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset, ImageAsset, Frame
-from math import floor
-
-myapp = App()
 
 
 
