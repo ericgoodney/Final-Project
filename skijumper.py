33e4c9f720d075981d0e31693072e64a9ee71357
ericgoodney <42844640+ericgoodney@users.noreply.github.com>
2018-12-07 12:44:55 -0500

    Updated from Brython Server: 12/7/2018 12:44:55 PM

--- skijumper.py
+++ skijumper.py
@@ -1,13 +1,12 @@
 """
-# Ski Jumper
-
 Fork this repository to begin working on your final project. Then do the following:
-
 * Edit the file called "functional.md" to describe the **functionality** of your project.
 * Edit the file called "design.md" to describe the **design** of your project.
 * Add your python sources.
 * When the project is finished, make  pull request to let me know you're finished.
 """
+from ggame import App, RectangleAsset, ImageAsset, Frame, SoundAsset
+from ggame import LineStyle, Color, Sprite, Sound
 from ggame import CircleAsset, LineStyle, Color, Sprite, App, RectangleAsset
 from math import sin,cos, tan, acos, asin, atan
 from math import exp, expm1, e, pi
@@ -24,9 +23,14 @@ orange = Color(0xFF7D40, 1.0)
 thinline1 = LineStyle(1, black)
 thinline = LineStyle(1, blue)
 
-ball_asset = ImageAsset("Python Skiier.png", Frame(62,210,275,275),3)
+skier_asset = ImageAsset("Python Skiier.png",Frame(20,20,600,600))
+skier = Sprite(skier_asset, (0, 0))
 
-
+"""
+skier.scale = 0.1
+skier.direction = 1
+skier.go = True
+"""
 
 for z in range(-15, 6):
     Ypoints = (-1*(z**2))+400
