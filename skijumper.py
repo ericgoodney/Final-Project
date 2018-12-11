fcfb60df3e8a57366e89201b609539626d66ee2f
ericgoodney <42844640+ericgoodney@users.noreply.github.com>
2018-12-11 12:56:05 -0500

    Updated from Brython Server: 12/11/2018 12:56:05 PM

--- skijumper.py
+++ skijumper.py
@@ -1,19 +1,4 @@
 """
-Fork this repository to begin working on your final project. Then do the following:
-* Edit the file called "functional.md" to describe the **functionality** of your project.
-* Edit the file called "design.md" to describe the **design** of your project.
-* Add your python sources.
-* When the project is finished, make  pull request to let me know you're finished.
-"""
-#g=9.81m/s^2
-#a=g*sinangle
-#h(x) = ax^2 + bx + c 
-#h'(x) = 2ax +b
-#def height(x):
-    #slope(x): 
-    
-
-
 from ggame import App, RectangleAsset, ImageAsset, Frame, SoundAsset
 from ggame import LineStyle, Color, Sprite, Sound
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
@@ -33,32 +18,41 @@ orange = Color(0xFF7D40, 1.0)
 thinline1 = LineStyle(1, black)
 thinline = LineStyle(1, black)
 
-skier_asset = ImageAsset("images/Python Skiier.png", Frame(0,0,685,685))
-skier = Sprite(skier_asset, (-30, 70))
-skier.scale = 0.2
 """
-skier.direction = 1
-skier.go = True
+
+
+
+print('Enter the folowing values of the domain and function y = ax^2 + bx + c of your ski jump: ')
+x1 = float(input('left domain = '))
+x2 = float(input('right domain = '))
+a = float(input('a = '))
+b = float(input('b = '))
+c = float(input('c = '))
+
+y = a*('x'**2) + b*'x' + c
+print(y)
+
+y1 = 2*a*x + b 
+
+
+
+
+
 """
-for z in range(-15, 6):
-    Ypoints = (-1*(z**2))+400
-    Xpoints = 20*(z+(150/8))
-    point = CircleAsset(4, thinline, black)
-    Sprite(point, (Xpoints, Ypoints))
-print(' ')
-
-rectangle1 = RectangleAsset(12, 700, thinline, black)
-
-Sprite(rectangle1, (63,175))
-
-rectangle2  = RectangleAsset(12,400, thinline, black)
-
-Sprite(rectangle2, (474,384))
-
-
-
-
-
-
-myapp = App()
+#g=9.81m/s^2
+#a=g*sinangle
+#h(x) = ax^2 + bx + c 
+#h'(x) = 2ax +b
+#def height(x):
+    #slope(x): 
+y=ax^2+bx+x
+y=2ax+ b
+x min
+x max
+a
+b
+c
+"""
+    
+#myapp = skijump()
 myapp.run()
